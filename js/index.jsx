cd3ad2babaeaa4817df620a1b867a142ae3c2490
cjjujubee <connie.jew@gmail.com>
2016-05-10 11:46:39 -0700

    Removes old code and renames project

--- js/index.jsx
+++ js/index.jsx
@@ -11,7 +11,7 @@ var TrelloCard = function(props) {
 };
 
 TrelloCard.defaultProps = {
-    text: 'Meekus',
+    text: 'Hello...Clarice :)',
     image: 'http://www-cs-faculty.stanford.edu/~uno/don.gif'
 };
 
@@ -35,49 +35,6 @@ var TrelloBoard = function() {
         </div>
     );
 };
-
-//var Person = React.createClass({
-
-//    getInitialState: function() {
-//        return {
-//            highlight: false,
-//            growIMG: false
-//        };
-//    },
-//    onClick: function() {
-//        this.setState({
-//            highlight: !this.state.highlight,
-//            growIMG: !this.state.growIMG
-//        });
-//    },
-//    render: function() {
-//        var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
-//        var growImage = 'person-img ' + (this.state.growIMG ? 'growIMG' : '');
-//        return (
-//            <div className={classes} onClick={this.onClick}>
-//                <div className="person-name">{this.props.name}</div>
-//                <img className={growImage} src={this.props.imageUrl} />
-//                <div className="person-job">
-//                    {this.props.job}
-//                </div>
-//            </div>
-//        );
-//    }
-//});
-//
-//var PersonList = function() {
-//    var text = 'Derek Zoolander';
-//    return (
-//        <div className="card">
-//            <div className="card-text">{text}</div>
-//        </div>
-//    );
-//
-//};
-//
-//Person.defaultProps = {
-//    imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
-//};
 
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
