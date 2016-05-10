8779dc06a75fb7a210cd139d46237b6ce1cc8c68
Kyle <kyleludlow@gmail.com>
2016-05-10 11:01:17 -0700

    pretty much trello

--- js/index.jsx
+++ js/index.jsx
@@ -1,49 +1,80 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = React.createClass({
-    getInitialState: function() {
-        return {
-            highlight: false,
-            growIMG: false
-        };
-    },
-    onClick: function() {
-        this.setState({
-            highlight: !this.state.highlight,
-            growIMG: !this.state.growIMG
-        });
-    },
-    render: function() {
-        var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
-        var growImage = 'person-img ' + (this.state.growIMG ? 'growIMG' : '');
-        return (
-            <div className={classes} onClick={this.onClick}>
-                <div className="person-name">{this.props.name}</div>
-                <img className={growImage} src={this.props.imageUrl} />
-                <div className="person-job">
-                    {this.props.job}
-                </div>
-            </div>
-        );
-    }
-});
-
-var PersonList = function() {
+var TrelloCard = function () {
+    var text = 'Not Derek Zoolander';
     return (
-        <div className="person-list">
-            <Person name="Derek Zoolander" imageUrl="http://uifaces.com/assets/static/images/zoolander.jpg" job="Male model"/>
-            <Person name="Donald Knuth" imageUrl="http://www-cs-faculty.stanford.edu/~uno/don.gif" job="Clever chap"/>
-            <Person name="Willy Wonka" imageUrl="https://s-media-cache-ak0.pinimg.com/favicons/758e0863abf72b9d780f8a4ef74a20496487dcdd80ca5d1d51ddd6f6.png?800153a7aff313b566d38d9e06e1ca38" job="Candy Maker/Children Killer?"/>
+        <li className="card">
+            <div className="card-text">{text}</div>
+        </li>
+    );
+};
+
+var TrelloList = function () {
+    return (
+        <ul>
+            <TrelloCard />
+            <TrelloCard />
+            <TrelloCard />
+            <TrelloCard />
+        </ul>
+    );
+};
+
+var TrelloBoard = function () {
+    return (
+        <div>
+            <TrelloList />
+            <TrelloList />
+            <TrelloList />
         </div>
     );
 };
 
-Person.defaultProps = {
-    imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
-};
+//var Person = React.createClass({
 
-document.addEventListener('DOMContentLoaded', function() {
+//    getInitialState: function() {
+//        return {
+//            highlight: false,
+//            growIMG: false
+//        };
+//    },
+//    onClick: function() {
+//        this.setState({
+//            highlight: !this.state.highlight,
+//            growIMG: !this.state.growIMG
+//        });
+//    },
+//    render: function() {
+//        var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
+//        var growImage = 'person-img ' + (this.state.growIMG ? 'growIMG' : '');
+//        return (
+//            <div className={classes} onClick={this.onClick}>
+//                <div className="person-name">{this.props.name}</div>
+//                <img className={growImage} src={this.props.imageUrl} />
+//                <div className="person-job">
+//                    {this.props.job}
+//                </div>
+//            </div>
+//        );
+//    }
+//});
+//
+//var PersonList = function() {
+//    var text = 'Derek Zoolander';
+//    return (
+//        <div className="card">
+//            <div className="card-text">{text}</div>
+//        </div>
+//    );
+//
+//};
+//
+//Person.defaultProps = {
+//    imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
+//};
+
+document.addEventListener('DOMContentLoaded', function () {
     ReactDOM.render(
-        <PersonList/>, document.getElementById('app'));
+        <TrelloBoard />, document.getElementById('app'));
 });
