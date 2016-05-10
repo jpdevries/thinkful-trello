04119d98614b82798c6b4e4a85d8077ff4de7732
cjjujubee <connie.jew@gmail.com>
2016-05-10 08:47:24 -0700

    Adds ability to blow up images on click

--- js/index.jsx
+++ js/index.jsx
@@ -1,25 +1,46 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = function(props) {
-    return (
-        <div className="person">
-            <div className="person-name">{props.name}</div>
-            <img className="person-img" src={props.imageUrl}/>
-            <div className="person-job">
-                {props.job}
+var Person = React.createClass({
+    getInitialState: function() {
+        return {
+            highlight: false,
+            growIMG: false
+        };
+    },
+    onClick: function() {
+        this.setState({
+            highlight: !this.state.highlight,
+            growIMG: !this.state.growIMG
+        });
+    },
+    render: function() {
+        var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
+        var growImage = 'person-img ' + (this.state.growIMG ? 'growIMG' : '');
+        return (
+            <div className={classes} onClick={this.onClick}>
+                <div className="person-name">{this.props.name}</div>
+                <img className={growImage} src={this.props.imageUrl} />
+                <div className="person-job">
+                    {this.props.job}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+});
 
 var PersonList = function() {
     return (
         <div className="person-list">
             <Person name="Derek Zoolander" imageUrl="http://uifaces.com/assets/static/images/zoolander.jpg" job="Male model"/>
             <Person name="Donald Knuth" imageUrl="http://www-cs-faculty.stanford.edu/~uno/don.gif" job="Clever chap"/>
+            <Person name="Donald Knuth" job="Clever chap"/>
         </div>
     );
+};
+
+Person.defaultProps = {
+    imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
 };
 
 document.addEventListener('DOMContentLoaded', function() {
